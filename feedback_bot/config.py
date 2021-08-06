98e89a196696b655b4dad54cc1ba8f27abc141ee
polfpilf <polfpilf@protonmail.com>
2021-08-06 23:02:53 +0200

    Add initial configuration

---
+++ feedback_bot/config.py
@@ -0,0 +1,19 @@
+
+from dynaconf import Dynaconf, Validator
+
+settings = Dynaconf(
+    settings_files=["settings.toml", ".secrets.toml"],
+    envvar_prefix="",
+    validators=[
+        Validator("TELEGRAM_BOT_TOKEN", must_exist=True, is_type_of=str, len_min=1),
+        Validator(
+            "TELEGRAM_WEBHOOK_PATH",
+            must_exist=True,
+            is_type_of=str,
+            len_min=1,
+            condition=lambda x: x.startswith("/"),
+        ),
+        Validator("HOST", must_exist=True, is_type_of=str, len_min=1),
+        Validator("PORT", must_exist=True, is_type_of=int, gt=1024, lt=65535),
+    ]
+)
