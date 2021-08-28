073d9ff349814fac001c507f3e31bb875ca1412e
polfpilf <polfpilf@protonmail.com>
2021-08-28 02:00:37 +0200

    Fix initial schema migration

--- alembic/versions/20210827003519-6ba2ed7df90f-initial_schema.py
+++ alembic/versions/20210827003519-6ba2ed7df90f-initial_schema.py
@@ -1,4 +1,6 @@
-"""Create target_chat and admin tables
+"""Create initial schema.
+
+Create admin, target_chat and forwarded_message tables.
 
 Revision ID: 6ba2ed7df90f
 Revises: 3fcf475aa60c
@@ -15,25 +17,36 @@ down_revision = None
 branch_labels = None
 depends_on = None
 
+_TARGET_CHAT_TABLE = "target_chat"
+_ADMIN_TABLE = "admin"
+_FORWARDED_MESSAGE_TABLE = "forwarded_message"
+
 
 def upgrade():
     op.create_table(
-        "target_chat",
+        _TARGET_CHAT_TABLE,
         sa.Column("chat_id", sa.BigInteger, primary_key=True),
         sa.Column("created_at", sa.DateTime(timezone=True), nullable=False),
     )
     op.create_table(
-        "admin",
+        _ADMIN_TABLE,
         sa.Column("user_id", sa.BigInteger, primary_key=True),
         sa.Column(
             "target_chat_id",
             sa.BigInteger,
             sa.ForeignKey("target_chat.chat_id"),
             nullable=False,
-        )
+        ),
+    )
+    op.create_table(
+        _FORWARDED_MESSAGE_TABLE,
+        sa.Column("forwarded_message_id", sa.Integer, primary_key=True),
+        sa.Column("target_chat_id", sa.BigInteger, primary_key=True),
+        sa.Column("origin_chat_id", sa.BigInteger, nullable=False),
     )
 
 
 def downgrade():
-    op.drop_table("admin")
-    op.drop_table("target_chat")
+    op.drop_table(_FORWARDED_MESSAGE_TABLE)
+    op.drop_table(_ADMIN_TABLE)
+    op.drop_table(_TARGET_CHAT_TABLE)
