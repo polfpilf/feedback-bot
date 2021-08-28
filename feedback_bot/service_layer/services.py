7c90d57c594904dab6bc1aced3256dd893444f7f
polfpilf <polfpilf@protonmail.com>
2021-08-28 03:15:05 +0200

    Fix admin reauthentication, uow field rename

--- feedback_bot/service_layer/services.py
+++ feedback_bot/service_layer/services.py
@@ -23,6 +23,10 @@ async def authenticate_admin(
     admin_token: str = Provide[Container.config.ADMIN_TOKEN],
 ):
     async with uow:
+        existing_admin = await uow.admins.get(user_id)
+        if existing_admin:
+            return
+
         admin = Admin.authenticate(
             user_id=user_id,
             chat_id=chat_id,
@@ -33,7 +37,7 @@ async def authenticate_admin(
             log.debug("User user_id: %d is not an admin", user_id)
             return
 
-        await uow.admin_repository.add(admin)
+        await uow.admins.add(admin)
         await uow.telegram_api.send_message(
             to_chat_id=admin.target_chat.chat_id,
             text="Auth token successfully verified",
@@ -48,15 +52,15 @@ async def add_group(
     uow: AbstractUnitOfWork = Provide[Container.uow],
 ):
     async with uow:
-        admin = await uow.admin_repository.get(user_id=by_user_id)
+        admin = await uow.admins.get(user_id=by_user_id)
         if not admin:
             log.debug("User user_id: %d is not an admin", by_user_id)
             return
 
         group_chat = TargetChat(chat_id=group_chat_id)
-        await uow.target_chat_repository.add(group_chat)
+        await uow.target_chats.add(group_chat)
 
-        admins = await uow.admin_repository.get_all()
+        admins = await uow.admins.get_all()
         notify_admin_coros = [
             uow.telegram_api.send_message(
                 to_chat_id=admin.target_chat.chat_id,
@@ -74,11 +78,11 @@ async def remove_group(
     uow: AbstractUnitOfWork = Provide[Container.uow]
 ):
     async with uow:
-        removed_group = await uow.target_chat_repository.remove(chat_id=group_chat_id)
+        removed_group = await uow.target_chats.remove(chat_id=group_chat_id)
         if not removed_group:
             return
 
-        admins = await uow.admin_repository.get_all()
+        admins = await uow.admins.get_all()
         notify_admin_coros = [
             uow.telegram_api.send_message(
                 to_chat_id=admin.target_chat.chat_id,
@@ -97,7 +101,7 @@ async def forward_incoming_message(
     uow: AbstractUnitOfWork = Provide[Container.uow],
 ):
     async with uow:
-        target_chat = await uow.target_chat_repository.get_latest()
+        target_chat = await uow.target_chats.get_latest()
         if not target_chat:
             return
         
@@ -111,7 +115,7 @@ async def forward_incoming_message(
             target_chat_id=target_chat.chat_id,
             origin_chat_id=origin_chat_id,
         )
-        await uow.forwarded_message_repository.add(forwarded_message)
+        await uow.forwarded_messages.add(forwarded_message)
         await uow.commit()
 
 
@@ -123,13 +127,13 @@ async def forward_reply(
     uow: AbstractUnitOfWork = Provide[Container.uow],
 ):
     async with uow:
-        forwarded_message = await uow.forwarded_message_repository.get(
+        forwarded_message = await uow.forwarded_messages.get(
             forwarded_message_id=forwarded_message_id,
             target_chat_id=target_chat_id,
         )
         if not forwarded_message:
             return
-        
+
         await uow.telegram_api.copy_message(
             from_chat_id=target_chat_id,
             to_chat_id=forwarded_message.origin_chat_id,
