a452c92e5a0ee01fb3caac0ef86749c088087636
polfpilf <polfpilf@protonmail.com>
2021-10-05 20:23:25 +0200

    Normalize heroku DB url

--- feedback_bot/config.py
+++ feedback_bot/config.py
@@ -1,6 +1,18 @@
 from dynaconf import Dynaconf, Validator
 
 
+def _normalize_database_url(url: str) -> str:
+    url = url.strip()
+
+    deprecated_schema = "postgres://"
+    new_schema = "postgresql://"
+
+    if url.startswith(deprecated_schema):
+        url = url.replace(deprecated_schema, new_schema, 1)
+    
+    return url
+
+
 settings = Dynaconf(
     settings_files=["settings.toml", ".secrets.toml"],
     envvar_prefix=False,
@@ -11,7 +23,13 @@ settings = Dynaconf(
         Validator("HOST", must_exist=True, is_type_of=str, len_min=1),
         Validator("PORT", must_exist=True, is_type_of=int, gt=1024, lt=65535),
         Validator("ADMIN_TOKEN", must_exist=True, is_type_of=str, len_min=8),
-        Validator("DATABASE_URL", must_exist=True, is_type_of=str),
+        Validator(
+            "DATABASE_URL",
+            must_exist=True,
+            is_type_of=str,
+            len_min=1,
+            cast=_normalize_database_url,
+        ),
     ]
 )
 
