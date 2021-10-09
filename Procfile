caf6a5b1bf16505c7c44b61d4818c6fbb0737ebe
flipflop <88234609+polfpilf@users.noreply.github.com>
2021-10-09 20:09:24 +0000

    Add Heroku integration (#1)
    * Return exit code on container start failure
    
    * Fix envvar_prefix removal
    
    * Add heroku application manifest
    
    * Add heroku Procfile
    
    * Add "Deploy to Heroku" button
    
    * Use poetry buildpack
    
    * Specify heroku python version
    
    * Disable runtime.txt generation
    
    * Try default python buildpack
    
    * Small var description edit
    
    * Fix web variable composition
    
    * Add / prefix to webhook path if it's missing
    
    * Preset env var for heroku processes
    
    * Normalize heroku DB url
    
    * Fix DB URL normalization
    
    * Fix DB URL update after normalization
    
    * Remove debug printing
    
    * Use pyproject buildback. Fix env variable
    
    * Fix webhook host value
    
    * Add python buildpack
    
    * Add webhook path normalization
    
    * Fix webhook path normalization

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: TELEGRAM_WEBHOOK_HOST="${HEROKU_APP_NAME}.herokuapp.com" alembic upgrade head
+web: TELEGRAM_WEBHOOK_HOST="${HEROKU_APP_NAME}.herokuapp.com" python feedback_bot
--- feedback_bot/config.py
+++ feedback_bot/config.py
@@ -1,9 +1,30 @@
-from dynaconf import Dynaconf, Validator
+from dynaconf import Dynaconf, LazySettings, Validator
+
+
+def _normalize_database_url(settings: LazySettings, validator: Validator):
+    url: str = settings.DATABASE_URL
+
+    deprecated_schema = "postgres://"
+    new_schema = "postgresql://"
+
+    if url.startswith(deprecated_schema):
+        url = url.replace(deprecated_schema, new_schema, 1)
+    
+    settings.DATABASE_URL = url
+
+
+def _normalize_webhook_path(settings: LazySettings, validator: Validator):
+    webhook_path: str = settings.TELEGRAM_WEBHOOK_PATH
+    
+    if not webhook_path.startswith("/"):
+        webhook_path = "/" + webhook_path
+
+    settings.TELEGRAM_WEBHOOK_PATH = webhook_path
 
 
 settings = Dynaconf(
     settings_files=["settings.toml", ".secrets.toml"],
-    envvar_prefix="",
+    envvar_prefix=False,
     validators=[
         Validator("TELEGRAM_BOT_TOKEN", must_exist=True, is_type_of=str, len_min=1),
         Validator(
@@ -11,14 +32,20 @@ settings = Dynaconf(
             must_exist=True,
             is_type_of=str,
             len_min=1,
-            condition=lambda x: x.startswith("/"),
+            default=_normalize_webhook_path
         ),
         Validator("TELEGRAM_WEBHOOK_HOST", must_exist=True, is_type_of=str, len_min=1),
         Validator("HOST", must_exist=True, is_type_of=str, len_min=1),
         Validator("PORT", must_exist=True, is_type_of=int, gt=1024, lt=65535),
         Validator("ADMIN_TOKEN", must_exist=True, is_type_of=str, len_min=8),
-        Validator("DATABASE_URL", must_exist=True, is_type_of=str),
-    ]
+        Validator(
+            "DATABASE_URL",
+            must_exist=True,
+            is_type_of=str,
+            len_min=1,
+            default=_normalize_database_url,
+        ),
+    ],
 )
 
 settings.validators.validate()
